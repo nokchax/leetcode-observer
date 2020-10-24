2163bbd9c12218c1a9d9f1323f219654ecf851d2
nokchax <nokchax@gmail.com>
2020-10-24 23:48:10 +0900

    env: local env configuration

--- src/main/java/com/nokchax/observer/leetcode/domain/user/User.java
+++ src/main/java/com/nokchax/observer/leetcode/domain/user/User.java
@@ -2,10 +2,19 @@ package com.nokchax.observer.leetcode.domain.user;
 
 import lombok.Data;
 
+import javax.jdo.annotations.Unique;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Table
+@Entity
 public class User {
+    @Id
     private String id;
     private String name;
+    @Unique
     private String telegramId;
 
     private int total;
