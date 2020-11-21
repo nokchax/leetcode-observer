01ece7daa369c076c5b2c57d0c8338e5ccc2d60e
nokchax <nokchax@gmail.com>
2020-11-21 23:34:54 +0900

    design: dispatcher servlet

--- src/main/java/com/nokchax/observer/leetcode/core/MessageDispatcher.java
+++ src/main/java/com/nokchax/observer/leetcode/core/MessageDispatcher.java
@@ -2,4 +2,10 @@ package com.nokchax.observer.leetcode.core;
 
 // dispatch message
 public class MessageDispatcher {
+
+    // get message and return response message
+    // get handler and handling message
+    // error catch
+    // return response message
+
 }
