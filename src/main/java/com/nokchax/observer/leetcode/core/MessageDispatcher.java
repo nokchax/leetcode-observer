835a324e12eeba766ee6f38bcdcf12d324e4853b
nokchax <nokchax@gmail.com>
2020-11-25 20:52:22 +0900

    design: message dispatcher

--- src/main/java/com/nokchax/observer/leetcode/core/MessageDispatcher.java
+++ src/main/java/com/nokchax/observer/leetcode/core/MessageDispatcher.java
@@ -4,8 +4,27 @@ package com.nokchax.observer.leetcode.core;
 public class MessageDispatcher {
 
     // get message and return response message
-    // get handler and handling message
     // error catch
-    // return response message
+    // make response message
+
+    /*
+    protected void service(final HttpServletRequest request, final HttpServletResponse response) {
+        try {
+            // get handler and handling message
+            Object handler = findHandler(request);
+
+            handle(handler, request, response);
+        } catch (NotFoundException e) {
+            logger.error("{}", e.getMessage());
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        } catch (InternalServerErrorException e) {
+            logger.error("{}", e.getMessage());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } catch (Throwable e) {
+            logger.error("Exception : {}", e);
+            throw new ServletException(e.getMessage());
+        }
+    }
+     */
 
 }
