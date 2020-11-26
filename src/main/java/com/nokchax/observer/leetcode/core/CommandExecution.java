d351b2d43b19c1cdf1af09e75f9c37b56afd6a4d
nokchax <nokchax@gmail.com>
2020-11-26 22:33:27 +0900

    design: command scanner and execution

---
+++ src/main/java/com/nokchax/observer/leetcode/core/CommandExecution.java
@@ -0,0 +1,9 @@
+package com.nokchax.observer.leetcode.core;
+
+public class CommandExecution {
+/*
+    private final Method method;
+    private final Object instance;
+    private final Parameters parameters;
+*/
+}
---
+++ src/main/java/com/nokchax/observer/leetcode/core/CommandScanner.java
@@ -0,0 +1,15 @@
+package com.nokchax.observer.leetcode.core;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class CommandScanner {
+    // scan commands
+
+    @PostConstruct
+    public void init() {
+
+    }
+}
