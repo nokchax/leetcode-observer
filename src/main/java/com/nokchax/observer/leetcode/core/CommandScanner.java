5f912d973e4212cee3291e4246292c7b99793dce
nokchax <nokchax@gmail.com>
2020-11-29 23:08:26 +0900

    todo: how to scan and return command classes

--- src/main/java/com/nokchax/observer/leetcode/core/CommandScanner.java
+++ src/main/java/com/nokchax/observer/leetcode/core/CommandScanner.java
@@ -5,8 +5,10 @@ import java.util.Set;
 public class CommandScanner {
     // scan commands
 
-    public static Set<Class<?>> scan() {
+    public static Set<Class<?>> scan(String... packages) {
+        // read Command annotation attached class from package
 
+        // make init class (like BeanDefinition)
         return null;
     }
 }
